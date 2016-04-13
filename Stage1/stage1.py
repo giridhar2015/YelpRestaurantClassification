ddf7721f3120ccf106beb820d6e9f4b7fe5bd30e
Chennakesava <manikanta019@gmail.com>
2016-04-12 20:42:08 -0400

    Adding stage1 changes

--- Stage1/stage1.py
+++ Stage1/stage1.py
@@ -1,3 +1,6 @@
+#stage1 is a feature extraction phase with each final feature vector of 4096 dimention
+#currently used fc7 layer 
+
 #path to caffe root
 caffe_root = '/home/mani/caffe/' 
 
@@ -17,7 +20,7 @@ if not os.path.isfile(caffe_root + 'models/bvlc_reference_caffenet/bvlc_referenc
 
 #module for image feature extraction
 def image_feature_extract(images, layer = 'fc7'):
-	#initialize the pretrainednetwork
+    #initialize the pretrainednetwork
     net = caffe.Net(caffe_root + 'models/bvlc_reference_caffenet/deploy.prototxt',
                 caffe_root + 'models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel',caffe.TEST)
     
@@ -36,10 +39,80 @@ def image_feature_extract(images, layer = 'fc7'):
 
     #calculate the no.of images
     num_images= len(images)
-    
+
     net.blobs['data'].reshape(num_images,3,227,227)
     net.blobs['data'].data[...] = map(lambda x: transformer.preprocess('data',caffe.io.load_image(x)), images)
     out = net.forward()
 
     return net.blobs[layer].data   
 
+#feature extraction starts here and results are stored in the form of h5 file format
+
+import h5py
+
+#store test images features to trainedimages_fc7_features.h5
+trainedImagesFeaturesFile = h5py.File(data_root+'trainedimages_fc7_features.h5','w')
+#setting filenames formats
+filenames = trainedImagesFeaturesFile.create_dataset('photo_id',(0,), maxshape=(None,),dtype='|S16')
+#set feature size
+feature = trainedImagesFeaturesFile.create_dataset('feature',(0,4096), maxshape = (None,4096))
+trainedImages.close()
+
+import pandas as pd 
+#read the train photos business ids
+train_photos = pd.read_csv(data_root+'train_photo_to_biz_ids.csv')
+train_folder = data_root+'train_photos/'
+#array for train images ids 
+train_pimages = [str(x) for x in train_photos['photo_id']]
+#array for train image paths
+train_images = [os.path.join(train_folder, str(x)+'.jpg') for x in train_photos['photo_id']]
+
+size_trainImages = len(train_images)
+#set batch size to 1000
+batch_size = 1000
+
+#start feature extraction for train images with given batch size
+for i in range(0, size_trainImages, batch_size): 
+    images = train_images[i: min(i+batch_size, size_trainImages)]
+    pimages = train_pimages[i: min(i+batch_size, size_trainImages)]
+    features = image_feature_extract(images, layer='fc7')
+    num_done = i+features.shape[0]
+    
+    f= h5py.File(data_root+'trainedimages_fc7_features.h5','r+')
+    f['photo_id'].resize((num_done,))
+    f['photo_id'][i: num_done] = np.array(pimages)
+    f['feature'].resize((num_done,features.shape[1]))
+    f['feature'][i: num_done, :] = features
+    f.close()
+
+    if num_done%10000==0 or num_done==size_trainImages:
+        print "Processed Train images: ", num_done
+
+#store test images features to testImages_fc7features.h5
+testImagesFeaturesFiles = h5py.File(data_root+'testImages_fc7features.h5','w')
+filenames = testImagesFeaturesFiles.create_dataset('photo_id',(0,), maxshape=(None,),dtype='|S54')
+feature = testImagesFeaturesFiles.create_dataset('feature',(0,4096), maxshape = (None,4096))
+testImagesFeaturesFiles.close()
+
+test_photos = pd.read_csv(data_root+'test_photo_to_biz.csv')
+test_folder = data_root+'test_photos/'
+test_pimages = [str(x) for x in test_photos['photo_id'].unique()]
+test_images = [os.path.join(test_folder, str(x)+'.jpg') for x in test_photos['photo_id'].unique()]  
+
+size_testImages = len(test_images)
+
+#start feature extraction for test images with given batch size
+for i in range(0, size_testImages, batch_size): 
+    images = test_images[i: min(i+batch_size, size_testImages)]
+    features = image_feature_extract(images, layer='fc7')
+    num_done = i+features.shape[0]
+    
+    f= h5py.File(data_root+'testImages_fc7features.h5','r+')
+    f['photo_id'].resize((num_done,))
+    f['photo_id'][i: num_done] = np.array(pimages)
+    f['feature'].resize((num_done,features.shape[1]))
+    f['feature'][i: num_done, :] = features
+    f.close()
+    
+    if num_done%10000==0 or num_done==size_testImages:
+        print "Processed Test images: ", num_done
